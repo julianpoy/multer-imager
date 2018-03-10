ba33e358a27a154a741a2fa3409a373e2ea31e0b
Julian Poyourow <julianpoy@users.noreply.github.com>
2018-03-10 10:18:18 -0800

    Convert to buffer after autoOrient, before resize

--- index.js
+++ index.js
@@ -57,11 +57,26 @@ S3Storage.prototype._handleFile = function(req, file, cb) {
     var s3options = self.options.s3;
     s3options.ContentType = contentType;
     var outStream = self.s3fs.createWriteStream(filePath, s3options);
-    gm(file.stream)
-      .autoOrient()
+//     gm(file.stream)
+//       .autoOrient()
+//       .resize(self.options.gm.width , self.options.gm.height , self.options.gm.options)
+//       .stream(self.options.gm.format || DEFAULT_FORMAT)
+//       .pipe(outStream);
+    
+    let img = gm(file.stream).autoOrient().noProfile()
+    .size((err, size) => {
+      console.log('size befor:', size);
+    })
+    .toBuffer((err, noExifImg) => {
+      gm(noExifImg)
+      .size((err, size) => {
+         console.log('size after:', size);
+      })
       .resize(self.options.gm.width , self.options.gm.height , self.options.gm.options)
       .stream(self.options.gm.format || DEFAULT_FORMAT)
       .pipe(outStream);
+    });
+
     outStream.on('error', cb);
     outStream.on('finish', function() {
       cb(null, {
